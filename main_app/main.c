6d3ff17a6a24a3f3b324e114393a9b14e285b13d
leejugy <lee09260@naver.com>
2025-08-02 01:32:49 +0900

    음원 제거;;

--- main_app/main.c
+++ main_app/main.c
@@ -6,7 +6,7 @@ int main()
     alsa_ctl_t alsa_clt = {0, };
     alsa_set_t alsa_set = {0, };
     alsa_set.control = ALSA_CONTROL_CHANGE_MUSIC;
-    sprintf(alsa_set.play_audio_path, "/root/Lemon.wav");
+    sprintf(alsa_set.play_audio_path, "/root/sarenian.wav");
     alsa_init(&alsa_clt);
     alsa_control_set(&alsa_clt, &alsa_set);
     while(1)
